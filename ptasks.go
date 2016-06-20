c93f5676abca01f8d580a42112262308bddb6e17
sahilg1 <sahil.gupta432@gmail.com>
2016-06-20 10:53:49 -0700

    Can be started and stopped from S3 bucket

--- ptasks.go
+++ ptasks.go
@@ -4,7 +4,10 @@ The program is written in golang.
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -16,15 +19,24 @@ var wg sync.WaitGroup
 //}
 
 func main() {
-	for {
-		wg.Add(10)
-		for i := 0; i < 10; i++ { //creates 10 million threads. this can be altered to put different load on the CPU
-			go calc() //calls thread to calculate the value of pi
-		}
-
-		wg.Wait()
+	res, err := http.Get("https://s3.amazonaws.com/sahgupta-booster-stress-test/s3_file.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+	scanner.Scan()
+	fmt.Println(scanner.Text())
 
+	if scanner.Text() == "Start" || scanner.Text() == "start" {
+		for {
+			wg.Add(10000000)
+			for i := 0; i < 10000000; i++ { //creates 10 million threads. this can be altered to put different load on the CPU
+				go calc() //calls thread to calculate the value of pi
+			}
+			wg.Wait()
+		}
+	}
 }
 
 func calc() { //function to calculate the value of pi
@@ -34,7 +46,7 @@ func calc() { //function to calculate the value of pi
 	var sign = 1.0
 	for k := 0; k < N; k++ {
 		term = float64((1.0) / (float64(2.0)*float64(k) + float64(1.0)))
-		time.Sleep(5 * time.Millisecond)
+		time.Sleep(2 * time.Millisecond)
 		sum = sum + float64(sign)*term
 		sign = -sign
 	}
