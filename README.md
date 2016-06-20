66bdfb22e71d072c0bc25aa22be86d8ccf48d0f9
sahilg1 <sahil.gupta432@gmail.com>
2016-06-20 15:32:30 -0700

    made changes to the way it can be stopped

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# docker_testing
+This application is used to apply stress on the containers and meant for testing in Booster. The app is written in Go programming language.
+
+The program creates 10 million threads which calculate the value of Pi in order to put load on the CPU and RAM.
+
+The application connects to the S3 bucket to check for the continuity of the program to stop it.
+https://s3.amazonaws.com/sahgupta-booster-stress-test/s3_file.txt
+
+The app can be stopped by editing the text file by typing Stop/stop and saving it.
--- ptasks.go
+++ ptasks.go
@@ -8,28 +8,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup
 
-//func init() {
-//runtime.GOMAXPROCS(runtime.NumCPU())
-//}
-
 func main() {
-	res, err := http.Get("https://s3.amazonaws.com/sahgupta-booster-stress-test/s3_file.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(res.Body)
-	defer res.Body.Close()
-	scanner.Scan()
-	fmt.Println(scanner.Text())
 
-	if scanner.Text() == "Start" || scanner.Text() == "start" {
-		for {
+	for {
+		if checkval() {
 			wg.Add(10000000)
 			for i := 0; i < 10000000; i++ { //creates 10 million threads. this can be altered to put different load on the CPU
 				go calc() //calls thread to calculate the value of pi
@@ -37,6 +26,7 @@ func main() {
 			wg.Wait()
 		}
 	}
+
 }
 
 func calc() { //function to calculate the value of pi
@@ -45,11 +35,31 @@ func calc() { //function to calculate the value of pi
 	var term float64
 	var sign = 1.0
 	for k := 0; k < N; k++ {
-		term = float64((1.0) / (float64(2.0)*float64(k) + float64(1.0)))
-		time.Sleep(2 * time.Millisecond)
-		sum = sum + float64(sign)*term
-		sign = -sign
+		if checkval() {
+			term = float64((1.0) / (float64(2.0)*float64(k) + float64(1.0)))
+			time.Sleep(5 * time.Millisecond)
+			sum = sum + float64(sign)*term
+			sign = -sign
+		} else {
+			os.Exit(3)
+		}
 	}
 	fmt.Println("Pi=", float64(sum*4.0))
 	wg.Done()
+}
+
+//This function checks the value of the S3 bucket to check to stop or continue running
+func checkval() bool {
+	res, err := http.Get("https://s3.amazonaws.com/sahgupta-booster-stress-test/s3_file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+	if scanner.Text() == "stop" || scanner.Text() == "Stop" {
+		return false
+	}
+	return true
 }
